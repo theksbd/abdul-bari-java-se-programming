5d9b519596ff0f08fd3be641c2d122112f2a9c13
Do Thien Hoang <theksbd@gmail.com>
2023-05-05 21:53:03 +0700

    update

---
+++ theksbd/src/TwoDimensionArrayPractice.java
@@ -0,0 +1,21 @@
+public class TwoDimensionArrayPractice {
+    public static void main(String[] args) {
+        // int[][] arr = new int[3][4];
+        int[][] arr;
+        arr = new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12, 13, 14 } };
+
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        for (int[] a : arr) {
+            for (int x : a) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
